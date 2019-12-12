37a9b7424600ce2368c3e281a90b0fa292bbbde7
Andy <winglive@163.com>
2019-12-12 21:50:05 +0800

    Redisson  lock

--- src/main/java/com/fancv/controller/RedisController.java
+++ src/main/java/com/fancv/controller/RedisController.java
@@ -1,13 +1,13 @@
 package com.fancv.controller;
 
 import com.fancv.service.RedisService;
+import com.fancv.service.impl.RedissonDistributedLocker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("redis/")
@@ -17,9 +17,36 @@ public class RedisController {
     @Autowired
     RedisService redisService;
 
+    @Autowired
+    RedissonDistributedLocker redissonDistributedLocker;
+
     @ApiOperation(value="获取用户列表", notes="")
     @GetMapping("get-key")
     public String getKey(@RequestParam String key) {
         return redisService.getKey(key);
     }
+
+
+
+
+    @RequestMapping(value = "hello2", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello2() throws InterruptedException {
+        int num = 1;
+        String lock = "lock_key";
+        try {
+            if (redissonDistributedLocker.tryLock(lock, TimeUnit.SECONDS, 5L, 10L)) {
+                // 处理逻辑
+                /*redisUtil.incr("test_num1");*/
+                // Thread.sleep(1000);
+                System.out.println("TEST1===========第 " + num + " 次请求===================" );
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            redissonDistributedLocker.unlock(lock);
+        }
+        return "success";
+    }
+
 }
---
+++ src/main/java/com/fancv/service/DistributedLocker.java
@@ -0,0 +1,21 @@
+package com.fancv.service;
+
+import org.redisson.api.RLock;
+
+import java.util.concurrent.TimeUnit;
+
+public interface DistributedLocker {
+
+    RLock lock(String lockKey);
+
+    RLock lock(String lockKey, long timeout);
+
+    RLock lock(String lockKey, TimeUnit unit, long timeout);
+
+    boolean tryLock(String lockKey, TimeUnit unit, long waitTime, long leaseTime);
+
+    void unlock(String lockKey);
+
+    void unlock(RLock lock);
+
+}
